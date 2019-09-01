7e41120b033cfc7e6eb7c921d3027260d68f0b02
Oleg S <xwx18@mail.ru>
2019-09-01 15:05:52 +0300

    part 3

--- js/script.js
+++ js/script.js
@@ -12,3 +12,26 @@ $('.question__question_title').on('click', function () {
   $(this).next().slideToggle();
   $(this).toggleClass('active');
 });
+
+const compactHover = (el, arr, isHover) => {
+  if (isHover) {
+    arr.find('.compact__item_title').removeClass('compact__item_title--active');
+    arr.find('.compact__item_descr').removeClass('compact__item_descr--active');
+    arr.find('img').removeClass('active');
+    el.find('.compact__item_title').addClass('compact__item_title--active');
+    el.find('.compact__item_descr').addClass('compact__item_descr--active');
+    el.find('img').addClass('active');
+  } else {
+    el.find('.compact__item_title').removeClass('compact__item_title--active');
+    el.find('.compact__item_descr').removeClass('compact__item_descr--active');
+    el.find('img').removeClass('active');
+  }
+}
+
+const compactItems = $('.compact__item');
+
+compactItems.hover(function() {
+  compactHover($(this), compactItems, true);
+}, function () {
+  compactHover($(this), compactItems, false);
+})
